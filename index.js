495177e7f239d8b9d8d6750957f420e0e38eeba9
Prideland-Okoi <prideland.okoi@gmail.com>
2022-06-24 04:44:31 +0100

    alert added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ let identity = "Prideland Okoi";
 let height = "175cm";
 let country = "Nigeria";
 
+alert(identity);
+alert(height);
+alert(country);
+
 document.write("My name is " + identity);
 document.write("<br>");
 document.write("<br>");
